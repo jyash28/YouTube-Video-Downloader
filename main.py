2c0964cbd1070c0af11d3a1b70246ac0c109daa6
jyash28 <jyash548@gmail.com>
2021-02-12 22:44:42 +0530

    first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 
-url = ' paste your url here'
+url = 'paste your video url '
 my_video = YouTube(url)
 
 
@@ -11,9 +11,7 @@ print(my_video.title)
 #get Thumbnail Image
 print(my_video.thumbnail_url)
 
-#get all the stream resolution for the 
-for stream in my_video.streams:
-    print(stream)
+
 
 #set stream resolution
 my_video = my_video.streams.get_highest_resolution()
